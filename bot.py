9e1ad16a5881eb1234ef16f032878b895c7bb428
maciek367 <maciek2baba2@gmail.com>
2023-07-25 21:14:28 +0200

    simple bot code

---
+++ bot.py
@@ -0,0 +1,11 @@
+import pyautogui as pg
+import time
+import keyboard
+import random
+
+
+while True:
+    time.sleep(1)
+    pg.moveTo(2200, 575, 0.1)
+    time.sleep(1)
+    pg.moveTo(2400, 650, 0.1)
