8348e040def2bbc34d906a4a7482df967242b6c0
maciek367 <maciek2baba2@gmail.com>
2023-07-28 21:32:13 +0200

    set up cookie bot

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import pyautogui as pg
 import time
-import keyboard
-import random
 
 
 while True:
-    time.sleep(1)
-    pg.moveTo(2200, 575, 0.1)
-    time.sleep(1)
-    pg.moveTo(2400, 650, 0.1)
+  pg.moveTo(1600, 600, 0, _pause=False) # cookie
+  time.sleep(10)
+  pg.moveTo(2900, 800, 0, _pause=False) # shipment
+  pg.moveTo(2900, 320, 2, _pause=False) # cursor
+  pg.moveTo(2900, 200, 0, _pause=False) #perks
+  time.sleep(0.5)
